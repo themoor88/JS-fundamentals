69c3b86c18476ff115121fe937c1a625b1ae56c0
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-02 17:00:05 -0400

    Finished exercise 2 for arrays.

--- scripts.js
+++ scripts.js
@@ -21,4 +21,45 @@ for (var key in animal) {
   }
 }
 
+var noiseArray = ['purr'];
+
+noiseArray.unshift('hiss');
+noiseArray.push('meow');
+
+noiseArray[3] = 'growl';
+
+console.log(noiseArray.length);
+
+console.log(noiseArray);
+
+animal.noises = noiseArray;
+
+var animals = [];
+
+animals.push(animal);
+
+var quackers = { username: 'DaffyDuck', tagline: 'Yippeee!', noises: ['quack', 'honk', 'sneeze', 'growl'] };
+
+animals[animals.length] = quackers;
+
+console.log(animals);
+
+var dog = {
+  username: 'Kobasu',
+  tagline: 'Such profile',
+  noises: ['bark', 'woof', 'wow!']
+}
+
+var honeyBadger = {};
+honeyBadger.username = 'Randall';
+honeyBadger.tagline = 'Honey badger dont care';
+honeyBadger.noises = ['screech', 'growl'];
+
+animals.push(dog, honeyBadger);
+
+console.log(animals.length);
+
+
+
+
 
