cb86e13c15b80edb27b74ce448c47e79fbc30314
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-02 18:44:22 -0400

    Finished exercise 3 for functions.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 //Use this file to implement your project
+// Exercise 1
 var animal = {};
 
 animal.username = "Daffyduck";
@@ -21,6 +22,7 @@ for (var key in animal) {
   }
 }
 
+// Exercise 2
 var noiseArray = ['purr'];
 
 noiseArray.unshift('hiss');
@@ -59,7 +61,71 @@ animals.push(dog, honeyBadger);
 
 console.log(animals.length);
 
+// Exercise 3
+function AnimalTestUser(username) {
+  var args = arguments.length;
+  var otherArgs = [];
+  if (args > 1) {
+    for (var i = 1; i < args; i++) {
+      otherArgs.push(arguments[i]);
+    }
+  }
 
+  return {
+    username: username,
+    otherArgs: otherArgs
+  };
+}
 
+var testSheep = AnimalTestUser('CottonBall', {'loves dancing': true}, [1, 2, 4]);
+console.log(testSheep);
 
+function AnimalCreator(username, species, tagline, noises) {
+  return {
+    username: username,
+    species: speecies,
+    tagline: tagline,
+    noises: noises,
+    friends: []
+  };
+}
 
+var sheep = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
+console.log(sheep);
+
+function addFriend(animal, friend) {
+  animal.friends.push(friend);
+}
+
+var cow = AnimalCreator('Moo', 'Cow', 'got milk?', ['moo', 'moooo']);
+console.log(cow);
+
+var llama = AnimalCreator('Zeny', 'llama', 'lllll', ['sdf', 'sdfsf']);
+console.log(llama);
+addFriend(sheep, cow);
+console.log(sheep)
+addFriend(sheep, llama);
+console.log(sheep);
+
+var myFarm = [sheep, cow, llama];
+addFriend(cow, sheep);
+addFriend(llama, cow);
+console.log(myFarm);
+
+function addMatchesArray(farm) {
+  for (var animal in farm) {
+    farm[animal].matches = [];
+  }
+}
+
+addMatchesArray(myfarm);
+console.log(myFarm[0]);
+
+function giveMatches(farm) {
+  for (var animal in farm) {
+    farm[animal].matches.push(farm[animal].friends[0]);
+  }
+}
+
+giveMatches(myFarm);
+console.log(myFarm[0]);
