bbf7eba3374c08b134454fb787543ec0d144aad7
Wajid Kagzi <wjkagzi@gmail.com>
2015-08-02 14:05:09 -0400

    Finished exercise 1 for objects.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
-//Use this file to implement Part One of your project
+//Use this file to implement your project
+var animal = {};
+
+animal.username = "Daffyduck";
+
+animal["tagline"] = "quack";
+
+var noises = [];
+
+animal.noises = noises;
+
+console.log(animal);
+
+var count = 0;
+for (var key in animal) {
+  count ++;
+  if (key === 'username') {
+    console.log('Hi my name is ' + animal[key]);
+  } else if (key === 'tagline') {
+    console.log('I like to say ' + animal[key]);
+  }
+}
 
 
